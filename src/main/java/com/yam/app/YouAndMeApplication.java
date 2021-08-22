6af8c2f9a9e0289e7c0d380dc46ce4a78e3a56bb
Rebwon <msolo021015@naver.com>
2021-08-22 17:02:24 +0900

    Project Settings

---
+++ src/main/java/com/yam/app/YouAndMeApplication.java
@@ -0,0 +1,13 @@
+package com.yam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAndMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAndMeApplication.class, args);
+    }
+
+}

4207f8b9d6c3e5abe0e2137ffa38c336b2365496
Rebwon <msolo021015@naver.com>
2021-08-25 15:29:43 +0900

    Add: Database, Data Access Settings

---
+++ src/main/java/com/yam/app/account/infrastructure/DatabaseConfiguration.java
@@ -0,0 +1,18 @@
+package com.yam.app.account.infrastructure;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .build();
+    }
+}

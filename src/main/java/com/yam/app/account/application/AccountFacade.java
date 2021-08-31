d466e8839a81a087098f2e1065dc2321ea8902c8
Rebwon <msolo021015@naver.com>
2021-08-31 11:48:12 +0900

    Add integration tests

--- src/main/java/com/yam/app/account/application/AccountFacade.java
+++ src/main/java/com/yam/app/account/application/AccountFacade.java
@@ -1,13 +1,27 @@
 package com.yam.app.account.application;
 
+import com.yam.app.account.domain.RegisterAccountProcessor;
 import com.yam.app.account.presentation.RegisterAccountRequest;
 import com.yam.app.account.presentation.RegisterAccountResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AccountFacade {
 
+    private final RegisterAccountProcessor processor;
+    private final AccountResponseTranslator translator;
+
+    public AccountFacade(RegisterAccountProcessor processor,
+        AccountResponseTranslator translator) {
+        this.processor = processor;
+        this.translator = translator;
+    }
+
     public RegisterAccountResponse register(RegisterAccountRequest request) {
-        return null;
+        var command = request.toCommand();
+        return translator.translate(
+            processor.process(command.getEmail(), command.getNickname(), command.getPassword()));
     }
 }
--- src/test/java/com/yam/app/account/infrastructure/MybatisAccountRepositoryTest.java
+++ src/test/java/com/yam/app/account/infrastructure/MybatisAccountRepositoryTest.java
@@ -5,12 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.yam.app.account.domain.Account;
 import com.yam.app.account.domain.AccountReader;
 import com.yam.app.account.domain.AccountRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class MybatisAccountRepositoryTest {
 
     @Autowired

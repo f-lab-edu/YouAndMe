cf97d3c6c54a60f67407e1e7d087acb264132277
Rebwon <msolo021015@naver.com>
2021-09-22 12:40:41 +0900

    It manages the storage and inquiry of sessions using an external session repository.
    - Enable Redis Http Session
    - Embedded Redis Settings
    - Refactor integration tests

---
+++ src/main/java/com/yam/app/common/configuration/EmbeddedRedisConfiguration.java
@@ -0,0 +1,34 @@
+package com.yam.app.common.configuration;
+
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Slf4j
+@Profile({"test", "local"})
+@Configuration
+public class EmbeddedRedisConfiguration {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}

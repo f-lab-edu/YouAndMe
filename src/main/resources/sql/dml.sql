6c5ada53a74efd8394b941bc5f2014bdb15df723
Rebwon <msolo021015@naver.com>
2021-09-25 12:21:33 +0900

    Add member db schema
    - add member db schema
    - rename queryMapper
    - extract ddl, dml script

---
+++ src/main/resources/sql/dml.sql
@@ -0,0 +1,7 @@
+insert into account(email, email_check_token, email_check_token_generated_at, email_verified,
+                    joined_at, last_modified_at, password, withdraw, role)
+values ('jiwonDev@gmail.com', 'emailchecktoken', now(), false, now(), now(), 'password!',
+        false, 'DEFAULT'),
+       ('loginCheck@gmail.com', 'emailchecktoken1', now(), true, now(), now(),
+        '$2a$10$EqbMbYB0vcZnuA5CClqa9uiLDnjA6pCjxn208ZchzA2q3ofqnkhcq',
+        false, 'DEFAULT');

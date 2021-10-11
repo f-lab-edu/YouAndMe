7ca557d4d3da419895db489725c244d33e942a64
JiwonDev <sylvester7412@gmail.com>
2021-10-11 11:52:05 +0900

    Refactor Code - Runtime Exception을 IllegalStateException으로 변경 - apache.commons.lang3 의존성 추가 - 문자열 테스트에서 apache.commons.lang3.RandomStringUtils 를 사용하도록 변경

--- src/test/java/com/yam/app/comment/presentation/CommentApiUri.java
+++ src/test/java/com/yam/app/comment/presentation/CommentApiUri.java
@@ -2,7 +2,7 @@ package com.yam.app.comment.presentation;
 
 public final class CommentApiUri {
 
-    public static final String CREATE_COMMENT = "/api/comments/create";
+    public static final String CREATE_COMMENT = "/api/comments/";
     public static final String UPDATE_COMMENT = "/api/comments/";
     public static final String DELETE_COMMENT = "/api/comments/";
 

5291eaae93d125c840cde9e5b94825ffdba2363b
Rebwon <msolo021015@naver.com>
2021-10-21 14:49:59 +0900

    Add SpringBootAdmin Monitoring

---
+++ src/main/java/com/yam/app/common/configuration/MonitoringConfiguration.java
@@ -0,0 +1,10 @@
+package com.yam.app.common.configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAdminServer
+public class MonitoringConfiguration {
+
+}

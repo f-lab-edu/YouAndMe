357e2c61d984b7e78b4fad8d0d786e4ef4a54445
Rebwon <msolo021015@naver.com>
2021-10-21 12:21:41 +0900

    Refactor spring session redis config

--- src/main/java/com/yam/app/common/configuration/RedisConfiguration.java
+++ src/main/java/com/yam/app/common/configuration/RedisConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -28,6 +29,11 @@ public class RedisConfiguration {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 
 }
